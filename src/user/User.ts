9a66799c5283b1e555b6c28df73afa6332124a66
Todd Clapham <todd.clapham@icloud.com>
2024-04-27 13:12:43 +0100

    user update

--- src/user/User.ts
+++ src/user/User.ts
@@ -1,17 +1,8 @@
-import ErrorMessage from "../util/ErrorMessage";
-import { HttpCode } from "../util/HttpCodes";
-
 export class User {
     constructor(
         public id: number,
         public email: string,
         public name: string,
         public isAdmin: boolean,
-    ) {
-
-    }
-
-    static get notFoundError() {
-        return new ErrorMessage(HttpCode.notFound, "User not found");
-    }
+    ) {}
 }

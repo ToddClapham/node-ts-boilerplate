26e398ae93794744bc2c8d0de9dee42132ae0a04
Todd Clapham <todd.clapham@icloud.com>
2024-09-19 21:33:23 +0100

    package.json imported differently in public route for CI/CD with GitHub actions

--- src/public/public.routes.ts
+++ src/public/public.routes.ts
@@ -1,8 +1,9 @@
 import * as express from "express";
 const router = express.Router();
-import packageJson from '../../package.json';
+import fs from 'fs';
+import path from 'path';
 
-const version_number = packageJson.version;
+const version_number = JSON.parse(fs.readFileSync(path.join(__dirname, '../../', 'package.json')).toString()).version;
 
 router.get('/version', (req, res, next) => {
     res.send({
